9b87fc4c2152634d041e5cad3dad189c635c6d0e
dawneraq <dawneraq@gmail.com>
2020-08-25 18:14:23 -0700

    use American date format 🇺🇸

--- utils/filters/date.js
+++ utils/filters/date.js
@@ -1,11 +1,11 @@
-const appendSuffix = n => {
+const appendSuffix = (n) => {
   var s = ['th', 'st', 'nd', 'rd'],
-    v = n % 100
-  return n + (s[(v - 20) % 10] || s[v] || s[0])
-}
+    v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+};
 
-module.exports = function(value) {
-  const dateObject = new Date(value)
+module.exports = function (value) {
+  const dateObject = new Date(value);
 
   const months = [
     'January',
@@ -20,10 +20,10 @@ module.exports = function(value) {
     'October',
     'November',
     'December',
-  ]
-  const dayWithSuffix = appendSuffix(dateObject.getDate())
+  ];
+  const dayWithSuffix = appendSuffix(dateObject.getUTCDate());
 
-  return `${dayWithSuffix} ${
+  return `${
     months[dateObject.getMonth()]
-  } ${dateObject.getFullYear()}`
-}
+  } ${dayWithSuffix}, ${dateObject.getFullYear()}`;
+};

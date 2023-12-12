296b079f00811d12fccc50e995af7e1d00672192
Andrew Aquino <dawneraq@gmail.com>
2023-12-11 23:13:57 -0800

    shorten hero copy

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,9 @@
 export const config = {
   name: 'Andrew Aquino',
   shortDesc:
-    "Frontend software engineer. Projects and notes on web development: JavaScript, CSS, design, and anything else I'm learning.",
+    'Frontend software engineer. Projects and notes on web development: CSS/animation, modern JavaScript, and interactive visual experiences.',
   url: 'https://aqandrew.com',
   resumePath: '/Andrew-Aquino-Resume.pdf',
-  lang: 'en',
   author: {
     name: 'Andrew Aquino',
     email: 'hi@aqandrew.com',
@@ -14,16 +13,4 @@ export const config = {
   },
   lookingForWork: false,
   paymentPointer: 'https://ko-fi.com/aqandrew',
-  syntaxTheme: 'prism-night-owl.css',
-  permalinkClass: ['permalink'],
-  iframeClass: ['iframes-wrapper'],
-  codeClass: ['code-wrapper'],
-  figureClass: ['figure'],
-  paths: {
-    src: 'src',
-    images: 'img',
-    blogdir: 'blog',
-    includes: 'components',
-    output: 'dist',
-  },
 };

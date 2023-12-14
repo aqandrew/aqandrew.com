f07f7d4f01c94f8c3aa27d26a507711eb1928b5d
Andrew Aquino <dawneraq@gmail.com>
2023-12-14 03:01:35 -0800

    Revert "Revert "switch from Eleventy to Astro""

---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+export const config = {
+  name: 'Andrew Aquino',
+  shortDesc:
+    'Frontend software engineer. Projects and notes on web development: CSS/animation, modern JavaScript, and interactive visual experiences.',
+  url: 'https://aqandrew.com',
+  resumePath: '/Andrew-Aquino-Resume.pdf',
+  author: {
+    name: 'Andrew Aquino',
+    email: 'hi@aqandrew.com',
+    twitter: 'aqandrew',
+    github: 'aqandrew',
+    codepen: 'aqandrew',
+  },
+  lookingForWork: false,
+  paymentPointer: 'https://ko-fi.com/aqandrew',
+};
